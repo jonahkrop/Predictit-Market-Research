2c288bfa2822eec0c6e30247900d25822bdc90f4
jonahkrop <67211009+jonahkrop@users.noreply.github.com>
2020-08-06 23:15:39 -0700

    better suited to handle different requests
    Can now handle requesting all states for senate or house polls. adds state and district (if applicable) to the resulting .csv.

--- scrape_538.py
+++ scrape_538.py
@@ -3,7 +3,9 @@ import predict_party
 import pandas as pd
 import numpy as np
 import re
+import unidecode
 
+from time import sleep
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium import common
@@ -33,10 +35,34 @@ def main(state, election):
 
             final_results = pd.concat([final_results, day_results], axis=0)
 
-            # sometimes web scraping duplicates polls, not sure why
-            results = final_results.drop_duplicates(keep='first')
+        # sometimes web scraping duplicates polls, not sure why
+        results = final_results.drop_duplicates(keep='first')
+        results = results.reset_index(drop=True)
 
-            results = results.reset_index(drop=True)
+        # 538 uses state name (and district) abbreviations in their polls.
+        # annoyingly, they use different ones for Senate and House polls.
+        if election == 'house':
+            new = zip(*results['location'].apply(extract_state_district))
+            results['location'], results['district'] = new
+
+        elif election == 'senate':
+            states = states_dict_senate()
+            results['location'] = results['location'].map(states)
+            results['district'] = 0
+
+        results = results[[
+            'election',
+            'location',
+            'district',
+            'poll_date',
+            'pollster',
+            'pollster_grade',
+            'poll_sample',
+            'voter_type',
+            'candidate',
+            'party',
+            'polling'
+            ]]
 
         # save to csv
         docname = '%s_%s_polling.csv' % (state, election)
@@ -66,7 +92,7 @@ def get_state_polling(state, election):
         to show more polls. Continue requesting more polls until we get
         through May.
         '''
-
+        
         # extract soup
         soup = BeautifulSoup(driver.page_source, 'lxml')
 
@@ -121,6 +147,16 @@ def extract_polling(poll_day):
     # extract publish date
     poll_date = poll_day.find_all('h2', {'class': 'day'})[0].attrs['data-date']
 
+    # extract election type (house, senate, etc) -- html isn't consistent
+    try:
+        elec = poll_day.find('td', {
+            'class': 'type hide-mobile single first'
+            }).text
+    except AttributeError:
+        elec = poll_day.find('td', {
+            'class': 'type hide-mobile single first last'
+            }).text
+
     # extract html for pollster names for all polls
     pollster_names = poll_day.find_all('td', {'class': 'pollster'})
 
@@ -134,11 +170,13 @@ def extract_polling(poll_day):
     poll_results = poll_day.find_all('td', {'class': 'answers hide-desktop'})
 
     result_columns = [
+        'election',
+        'location',
         'poll_date',
         'pollster',
         'pollster_grade',
-        'poll sample',
-        'voter type',
+        'poll_sample',
+        'voter_type',
         'candidate',
         'party',
         'polling'
@@ -169,12 +207,13 @@ def extract_polling(poll_day):
         except IndexError:
             pollster_grade = np.nan
 
-        # extract strings of poll information (sample, voter type)
+        # extract strings of poll information (sample, voter type, election)
         # html has only closed <br> tags, so replace w/ commas to separate
         poll_info = poll_infos[j]
         [br.replace_with(', ') for br in poll_info.select('br')]
         sample = poll_info.text.split(', ')[-1].split(' ')[0]
         voter = poll_info.text.split(', ')[-1].split(' ')[1]
+        location = poll_info.find('span').text.strip(' ')
 
         # extract strings of candidate + polling and parse
         poll_result = poll_results[j].text.split('%')[:-1]
@@ -192,9 +231,11 @@ def extract_polling(poll_day):
         temp_results['poll_date'] = poll_date
         temp_results['pollster'] = pollster_name
         temp_results['pollster_grade'] = pollster_grade
-        temp_results['poll sample'] = sample
-        temp_results['voter type'] = voter
+        temp_results['poll_sample'] = sample
+        temp_results['voter_type'] = voter
         temp_results['party'] = party
+        temp_results['election'] = elec
+        temp_results['location'] = location
 
         day_results = pd.concat([day_results, temp_results], axis=0)
 
@@ -212,6 +253,9 @@ def candidate_polling(poll_result):
 
         # remove any non-alphanumerics bc they fuck it up
         res = re.sub(r'\W+', '', poll_result[c])
+
+        # change any accented characters to regular
+        res = unidecode.unidecode(res)
 
         # extract letters and numbers from string into name + polling
         pull = extract_text_int(res)
@@ -255,16 +299,154 @@ def extract_text_int(string):
     Extract the text and integers from a string and separate them.
 
     '''
+
     r = re.compile("([a-zA-Z]+)([0-9]+)")
     sep = r.match(string)
 
     return sep
 
 
+def extract_state_district(location):
+    '''
+    For House elections, split state-district abbrev. into name and number.
+
+    '''
+
+    # split by hyphen
+    state_code = location.split('-')[0]
+    district = location.split('-')[1]
+
+    # use dict of shortcuts and names to grab full name
+    states = states_dict_house()
+    state_name = states[state_code]
+
+    return state_name, district
+
+
+def states_dict_house():
+    '''
+    Return dictionary of 538 House abbreviations to full name.
+    '''
+
+    states = {}
+    states['AL'] = 'alabama'
+    states['AK'] = 'alaska'
+    states['AZ'] = 'arizona'
+    states['AR'] = 'arkansas'
+    states['CA'] = 'california'
+    states['CO'] = 'colorado'
+    states['CT'] = 'connecticut'
+    states['DE'] = 'delaware'
+    states['FL'] = 'florida'
+    states['GA'] = 'georgia'
+    states['HI'] = 'hawaii'
+    states['ID'] = 'idaho'
+    states['IL'] = 'illinois'
+    states['IN'] = 'indiana'
+    states['IA'] = 'iowa'
+    states['KS'] = 'kansas'
+    states['KY'] = 'kentucky'
+    states['LA'] = 'louisiana'
+    states['ME'] = 'maine'
+    states['MD'] = 'maryland'
+    states['MA'] = 'massachusetts'
+    states['MI'] = 'michigan'
+    states['MN'] = 'minnesota'
+    states['MS'] = 'mississippi'
+    states['MO'] = 'missouri'
+    states['MT'] = 'montana'
+    states['NE'] = 'nebraska'
+    states['NV'] = 'nevada'
+    states['NH'] = 'new hampshire'
+    states['NJ'] = 'new jersey'
+    states['NM'] = 'new mexico'
+    states['NY'] = 'new york'
+    states['NC'] = 'north carolina'
+    states['ND'] = 'north dakota'
+    states['OH'] = 'ohio'
+    states['OK'] = 'oklahoma'
+    states['OR'] = 'oregon'
+    states['PA'] = 'pennsylvania'
+    states['RI'] = 'rhode island'
+    states['SC'] = 'south carolina'
+    states['SD'] = 'south dakota'
+    states['TN'] = 'tennessee'
+    states['TX'] = 'texas'
+    states['UT'] = 'utah'
+    states['VT'] = 'vermont'
+    states['VA'] = 'virginia'
+    states['WA'] = 'washington'
+    states['WV'] = 'west virginia'
+    states['WI'] = 'wisconsin'
+    states['WY'] = 'wyoming'
+
+    return states
+
+
+def states_dict_senate():
+    '''
+    Return dictionary of 538 Senate abbreviations to full name.
+    '''
+
+    states = {}
+    states['Ala.'] = 'alabama'
+    states['Alaska'] = 'alaska'
+    states['Ariz.'] = 'arizona'
+    states['AR'] = 'arkansas'
+    states['Calif.'] = 'california'
+    states['Colo.'] = 'colorado'
+    states['Conn.'] = 'connecticut'
+    states['Del.'] = 'delaware'
+    states['Fla.'] = 'florida'
+    states['Ga.'] = 'georgia'
+    states['HI'] = 'hawaii'
+    states['ID'] = 'idaho'
+    states['Ill.'] = 'illinois'
+    states['Ind.'] = 'indiana'
+    states['Iowa'] = 'iowa'
+    states['Kan.'] = 'kansas'
+    states['Ky.'] = 'kentucky'
+    states['LA'] = 'louisiana'
+    states['Maine'] = 'maine'
+    states['Md.'] = 'maryland'
+    states['Mass.'] = 'massachusetts'
+    states['Mich.'] = 'michigan'
+    states['Minn.'] = 'minnesota'
+    states['Miss.'] = 'mississippi'
+    states['Mo.'] = 'missouri'
+    states['Mont.'] = 'montana'
+    states['Neb.'] = 'nebraska'
+    states['Nev.'] = 'nevada'
+    states['N.H.'] = 'new hampshire'
+    states['N.J.'] = 'new jersey'
+    states['N.M.'] = 'new mexico'
+    states['N.Y.'] = 'new york'
+    states['N.C.'] = 'north carolina'
+    states['N.D.'] = 'north dakota'
+    states['Ohio'] = 'ohio'
+    states['Okla.'] = 'oklahoma'
+    states['OR'] = 'oregon'
+    states['Pa.'] = 'pennsylvania'
+    states['R.I.'] = 'rhode island'
+    states['S.C.'] = 'south carolina'
+    states['S.D.'] = 'south dakota'
+    states['Tenn.'] = 'tennessee'
+    states['Texas'] = 'texas'
+    states['Utah'] = 'utah'
+    states['Vt.'] = 'vermont'
+    states['Va.'] = 'virginia'
+    states['Wash.'] = 'washington'
+    states['W.Va.'] = 'west virginia'
+    states['Wis.'] = 'wisconsin'
+    states['Wyo.'] = 'wyoming'
+
+    return states
+
+
 if __name__ == "__main__":
         
     # set state & election type
-    state = 'iowa'
-    election = 'senate'
+    state = ''
+    election = 'house'
     
     main(state, election)
